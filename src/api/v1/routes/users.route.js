9761138338ba31c28832c3d9102b2e7d5a8a8f5f
Henrique Fonseca <henrique.fonseca@carely.pt>
2022-07-11 02:28:04 +0100

    Added production and development environments

--- src/api/v1/routes/users.route.js
+++ src/api/v1/routes/users.route.js
@@ -2,7 +2,7 @@ import express from "express"
 
 const router = express.Router()
 
-router.route("/").get((req, res) => res.send("Hello world"))
+router.route("/users").get((req, res) => res.send("Hello world"))
 
 
 export default router
--- src/server.js
+++ src/server.js
@@ -1,10 +1,42 @@
+/*  
+ * [EN]
+ *
+ * Copyright (C) Carely, LDA - All Rights Reserved
+ *
+ * Unauthorized copying or distributing of this file via any medium is strictly prohibited.
+ * This file is confidential and intellectual property of Carely, Lda.
+ * Contact: contact@carely.pt
+ * 
+ * © 2022 Carely. All Rights Reserved.
+ * 
+ * 
+ * [PT]
+ * 
+ * Copyright (C) Carely, LDA - Todos os direitos reservados
+ *
+ * A cópia ou distribuição não autorizada deste ficheiro por qualquer meio é estritamente proibida.
+ * Este ficheiro é confidencial e parte da propriedade intelectual da Carely, Lda.
+ * Contacto: contact@carely.pt
+ * 
+ * © 2022 Carely. Todos os direitos reservados. 
+ */
+
+
 import express from "express"
 import cors from "cors"
 import users from "./api/v1/routes/users.route.js"
 import dotenv from "dotenv"
 
+
+
 "Loads environment variables"
-dotenv.config({path: '../.env' }) 
+dotenv.config({path: './src/config/.env/.env'}) 
+const env = process.env.NODE_ENV
+const api_url = process.env.API_URL.concat(process.env.API_VERSION)
+dotenv.config({path: `./src/config/.env/.env.${env}`}) 
+
+
+
 
 const app = express()
 
@@ -14,7 +46,7 @@ app.use(express.json())
 //app.use(process.env.API_URL, caregivers)
 
 
-app.use(process.env.API_URL, users)
+app.use(api_url, users)
 app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
 
 export default app
