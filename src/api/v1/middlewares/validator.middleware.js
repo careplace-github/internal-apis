45a527e5d2e8028060d005840e42b816c50ec40d
Henrique Fonseca <henrique@fonseca.email>
2022-07-14 15:46:20 +0100

    Wordked on user registration input validations and user controller now checks if a user already exists before creating one

--- src/api/v1/middlewares/validator.middleware.js
+++ src/api/v1/middlewares/validator.middleware.js
@@ -1,12 +1,16 @@
 //import registerUserValitation from "./validators/users.validator"
 import {validationResult} from "express-validator"
+import User from "../models/users.model.js"
 
-const validatorMiddleware = (req, res, next) => {
+const validatorMiddleware = async (req, res, next) => {
+
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         res.status(500).json({
             errors: errors.array()
+        
         })
+       return
     }
     next()
 }
