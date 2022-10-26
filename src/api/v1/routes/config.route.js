dd3a0490e9ed8314c18d2603a33b2a2189e25f7b
Henrique Fonseca <henrique@fonseca.email>
2022-10-26 15:37:42 +0100

    Created script to initialize the server at reboot - EC2

--- src/api/v1/routes/config.route.js
+++ src/api/v1/routes/config.route.js
@@ -3,7 +3,7 @@ import express from "express"
 import AuthenticationController from "../controllers/authentication.controller.js"
 import UsersController from "../controllers/users.controller.js"
 
-import {registerUserValidation} from "../validators/signup.validator.js"
+import registerUserValidation from "../validators/signup.validator.js"
 import validatorMiddleware from "../middlewares/validator.middleware.js"
 
 
