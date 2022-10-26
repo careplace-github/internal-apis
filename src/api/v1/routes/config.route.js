a8975fd143cc691912d01d4fdc438fbb11dbadb9
Henrique Fonseca <henrique@fonseca.email>
2022-10-26 16:50:09 +0100

    Updated initServer.sh script

--- src/api/v1/routes/config.route.js
+++ src/api/v1/routes/config.route.js
@@ -3,8 +3,6 @@ import express from "express"
 import AuthenticationController from "../controllers/authentication.controller.js"
 import UsersController from "../controllers/users.controller.js"
 
-import registerUserValidation from "../validators/signup.validator.js"
-import validatorMiddleware from "../middlewares/validator.middleware.js"
 
 
 const router = express.Router()
