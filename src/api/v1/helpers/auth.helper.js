62c158b938372ecc1c9f120574026d1ecfe8fcba
Henrique Fonseca <henrique.fonseca@carely.pt>
2022-11-08 23:48:33 +0000

    Added API gateway /users/{userId}

--- src/api/v1/helpers/auth.helper.js
+++ src/api/v1/helpers/auth.helper.js
@@ -5,12 +5,12 @@ import AmazonCognitoIdentity from "amazon-cognito-identity-js"
 
 import CognitoService from "../services/cognito.service.js"
 
-import {AWS_user_pool_id} from "../../../config/constants/index.js"
-import {AWS_client_id} from "../../../config/constants/index.js"
-import {AWS_region} from "../../../config/constants/index.js"
-import {AWS_identity_pool_id} from "../../../config/constants/index.js"
-import {AWS_access_key_id} from "../../../config/constants/index.js"
-import {AWS_secret_access_key} from "../../../config/constants/index.js"
+import { AWS_user_pool_id } from "../../../config/constants/index.js"
+import { AWS_client_id } from "../../../config/constants/index.js"
+import { AWS_region } from "../../../config/constants/index.js"
+import { AWS_identity_pool_id } from "../../../config/constants/index.js"
+import { AWS_access_key_id } from "../../../config/constants/index.js"
+import { AWS_secret_access_key } from "../../../config/constants/index.js"
 
 import usersDAO from "../db/usersDAO.js"
 
@@ -28,55 +28,70 @@ export default class AuthHelper {
 
   static async isLoggedIn(token) {
 
-    const decodedToken = this.decodeToken(token);
+    try {
 
-    const currentTime = new Date().getTime() / 1000;
+      const decodedToken = this.decodeToken(token);
 
-    return decodedToken.exp > currentTime
+      const currentTime = new Date().getTime() / 1000;
+
+      return decodedToken.exp > currentTime
+    }
+
+    catch (e) {
+      return false
+    }
 
 
   }
 
- 
+
 
 
   static async getUserId(token, authProvider) {
 
-    let user
-    let userId
+    try {
+
+      let user
+      let userId
 
 
-    const decodedToken = await this.decodeToken(token);
+      const decodedToken = await this.decodeToken(token);
 
 
- 
 
 
-    switch (authProvider) {
-      case 'cognito':
-       
-      console.log("AUTH ID: " + decodedToken.sub)
 
-         user = await usersDAO.getUserByAuthId(decodedToken.sub, 'cognito')
+      switch (authProvider) {
+        case 'cognito':
 
-        userId = user._id
 
-        return userId
-        
 
-      default:
-         user = await usersDAO.getUserByAuthId(decodedToken.sub, 'cognito')
+          user = await usersDAO.getUserByAuthId(decodedToken.sub, 'cognito')
 
-        userId = user._id
+          userId = user._id
 
-        return userId
+          return userId
+
+
+        default:
+          user = await usersDAO.getUserByAuthId(decodedToken.sub, 'cognito')
+
+          userId = user._id
+
+          return userId
+
+      }
+
 
     }
-    
-    
+
+    catch (e) {
+      return null
+    }
+
   }
 
-  
-  
+
+
 
 }
--- src/api/v1/middlewares/validator.middleware.js
+++ src/api/v1/middlewares/validator.middleware.js
@@ -1,12 +1,12 @@
 //import registerUserValitation from "./validators/users.validator"
-import {validationResult} from "express-validator"
+import { validationResult } from "express-validator"
 import User from "../models/auth/user.model.js"
 
 const validatorMiddleware = async (req, res, next) => {
 
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-       return res.status(500).json({
+        return res.status(500).json({
             errors: errors.array()
         })
     }
