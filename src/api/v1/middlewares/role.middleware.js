543cc8980eedb6c35da9bf14ac6f1a9edd42cd0a
Henrique Fonseca <henrique@fonseca.email>
2022-11-10 00:20:19 +0000

    Added CRUD gateway for Companies

--- src/api/v1/middlewares/role.middleware.js
+++ src/api/v1/middlewares/role.middleware.js
@@ -1,84 +1,52 @@
-import AWS from "aws-sdk"
-import jwt_decode from "jwt-decode"
-import AmazonCognitoIdentity from "amazon-cognito-identity-js"
-import cognitoExpress from "cognito-express"
+import AWS from "aws-sdk";
+import jwt_decode from "jwt-decode";
+import AmazonCognitoIdentity from "amazon-cognito-identity-js";
+import cognitoExpress from "cognito-express";
 
-import CognitoService from "../services/cognito.service.js"
+import CognitoService from "../services/cognito.service.js";
 
-import {AWS_user_pool_id} from "../../../config/constants/index.js"
-import {AWS_client_id} from "../../../config/constants/index.js"
-import {AWS_region} from "../../../config/constants/index.js"
-import {AWS_identity_pool_id} from "../../../config/constants/index.js"
+import { AWS_user_pool_id } from "../../../config/constants/index.js";
+import { AWS_client_id } from "../../../config/constants/index.js";
+import { AWS_region } from "../../../config/constants/index.js";
+import { AWS_identity_pool_id } from "../../../config/constants/index.js";
 
-import AwsConfig from "../helpers/cognito.helper.js"
-import usersDAO from "../db/usersDAO.js"
-
-
-
+import AwsConfig from "../helpers/cognito.helper.js";
+import usersDAO from "../db/usersDAO.js";
 
 let cognitoAttributeList = [];
 
-
-export default function validateRole(role) {
-
+// @param {array} roles - array of roles
+export default function validateRole(roles) {
   return function (req, res, next) {
+    // Check that the request contains a token
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.split(" ")[0] === "Bearer"
+    ) {
+      const token = req.headers.authorization.split(" ")[1];
 
+      const decodedToken = AwsConfig.decodeJWTToken(token);
 
-  // Check that the request contains a token
-  if (req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
-    
-    const token = req.headers.authorization.split(" ")[1]
+      console.log("decodedToken: " + JSON.stringify(decodedToken));
 
-    const decodedToken = AwsConfig.decodeJWTToken(token);
+      // Asynchronously get the user from the database
+      usersDAO.getUserByAuthId(decodedToken.sub, "cognito").then((user) => {
+        const userRole = user.role;
 
-    console.log("decodedToken: " + JSON.stringify(decodedToken))
+        console.log("userRole: " + userRole);
 
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i] === userRole) {
+            return next();
+          }
+        }
 
-    // Asynchronously get the user from the database
-    usersDAO.getUserByEmail(decodedToken.email).then((user) => {
-
-    
-
-    let userRole
-    
-    usersDAO.getUserRoleByCognitoId(decodedToken.sub).then((userRole) => {
-
-      console.log("userRole: " + userRole)
-    
-      if (userRole === role) {
-        // User is authorized
-        next()
-      }
-
-      else {
         // User is not authorized
-        res.status(403).send("Forbidden")
-      }
-    })
-  })
-
-    
+        return res.status(403).send("Forbidden");
+      });
+    } else {
+      // If there is no token, respond appropriately
+      res.status(401).send("No token provided.");
+    }
+  };
 }
-
-else {
-  // If there is no token, respond appropriately 
-  res.status(401).send("No token provided.")
-}
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
