01124f9108246e007550373f7bebab5067eb1802
Henrique Fonseca <henrique.fonseca@careplace.pt>
2023-01-20 00:51:27 +0000

    Debuged '/users/account' endpoint

--- src/api/v1/routes/users.route.js
+++ src/api/v1/routes/users.route.js
@@ -12,15 +12,11 @@ const router = express.Router();
 
 router.route("/users")
   .get(UsersController.index)
-  .post(UsersController.addUserToMongoDb);
+ 
 
 router.route("/users/account")
   .get( UsersController.account);
 
-router.route("/users/:id")
-  .get(UsersController.show)
-  .patch(UsersController.update)
-  .put(UsersController.update)
-  .delete(UsersController.destroy);
+
 
 export default router;
