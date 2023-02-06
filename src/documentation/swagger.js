d07364c63c7236defa91a390abe5c9fa7fced042
Henrique Fonseca <henrique@fonseca.email>
2023-02-06 15:38:12 +0000

    Testing deployment

--- src/documentation/swagger.js
+++ src/documentation/swagger.js
@@ -2,30 +2,25 @@ import Express from "express";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 //import { version } from "../../package.json" assert { type: "json" };
-import { API_VERSION} from "../config/constants/index.js"
+import { API_VERSION } from "../config/constants/index.js";
 import logger from "../logs/logger.js";
 
 import YAML from "yamljs";
-
 
 const swaggerSpec = YAML.load("./src/documentation/swagger-stag.yaml");
 
 function swaggerDocs(app, port) {
   // Swagger page
   app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  
 
   app.get("docs.json", (req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.send(swaggerSpec);
   });
 
-
-
-  
-  
-
-  logger.info(`Swagger docs available at http://localhost:${port}/api/v1/docs \n`);
+  logger.info(
+    `Swagger docs available at http://localhost:${port}/api/v1/docs \n`
+  );
 }
 
 export default swaggerDocs;
