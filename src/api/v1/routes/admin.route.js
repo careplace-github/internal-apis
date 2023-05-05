95f939c10074de035639262b06b1c7f262a623ef
Henrique Fonseca <henrique@fonseca.email>
2023-05-05 20:31:26 +0100

    Added feature to query companies by location (set of coordinates)

--- src/api/v1/routes/admin.route.js
+++ src/api/v1/routes/admin.route.js
@@ -8,33 +8,22 @@ import AuthenticationGuard from "../middlewares/guards/authenticationGuard.middl
 
 // Import Controller
 import CompaniesController from "../controllers/companies.controller.js";
-import AuthController from "../controllers/authentication.controller";
 import ServicesController from "../controllers/services.controller.js";
 
 const router = express.Router();
 
-router.route("/admin/auth/login")
-.post(AuthController.adminLogin);
+router
+  .route("/admin/companies")
+  .get(CompaniesController.searchCompanies)
+  .post(CompaniesController.create);
 
-router.route("/admin/auth/logout")
-.post(AuthController.logout);
+router
+  .route("/admin/companies/:id")
+  .get(CompaniesController.retrieve)
+  .put(CompaniesController.update);
 
-router.route("/admin/companies")
-.get(CompaniesController.searchCompanies)
-.post(CompaniesController.create);
+router.route("/admin/services").post(ServicesController.create);
 
-router.route("/admin/companies/:id")
-.get(CompaniesController.retrieve)
-.put(CompaniesController.update);
-
-router.route("/admin/services")
-.get(ServicesController.searchServices)
-.post(ServicesController.create);
-
-router.route("/admin/services/:id")
-.get(ServicesController.retrieve)
-.put(ServicesController.update);
-
-
+router.route("/admin/services/:id").put(ServicesController.update);
 
 export default router;
