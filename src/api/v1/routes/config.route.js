f0df8884a91e841671b4ca6335371b0b707e68c4
Henrique Fonseca <henrique@fonseca.email>
2023-05-06 18:50:48 +0100

    Update config.route.js

--- src/api/v1/routes/config.route.js
+++ src/api/v1/routes/config.route.js
@@ -2,8 +2,13 @@ import express from "express";
 
 const router = express.Router();
 
-router.route("/api/v1").get(function (req, res) {
-  res.send("<html><h1> [DEVELOPMENT ENVIRONMENT] API V1.0.0 </h1></html>", 200);
+import  { ENV } from "../../../config/constants/index.js";
+
+router.route("/v1").get(function (req, res) {
+  res.send(
+    `<html><h1> [${ENV.toUpperCase()} ENVIRONMENT] API V1.0.0 </h1></html>`,200
+  );
 });
+
 
 export default router;
