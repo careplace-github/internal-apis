637e0fe0bc96f6ef48045bd9b3cfa68b65b5331d
Henrique Fonseca <henrique@fonseca.email>
2023-05-15 01:41:20 +0100

    Minor changes

--- src/api/v1/routes/config.route.js
+++ src/api/v1/routes/config.route.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
-router.route("/api/v1").get(function (req, res) {
+router.route("/v1").get(function (req, res) {
   res.send("<html><h1> [DEVELOPMENT ENVIRONMENT] API V1.0.0 </h1></html>", 200);
 });
 
