a2c76614f390d1cf2e7e93f5c8ace9279a220d72
Henrique Fonseca <henrique@fonseca.email>
2023-05-27 00:47:26 +0100

    Update companies.route.ts

--- src/api/v1/routes/companies.route.ts
+++ src/api/v1/routes/companies.route.ts
@@ -44,9 +44,11 @@ router
   .get(AuthenticationGuard, AccessGuard('crm'), StripeController.retrieveExternalAccount)
   .delete(AuthenticationGuard, AccessGuard('crm'), StripeController.deleteExternalAccount);
 
+router.route('/companies/:id').get(CompaniesController.retrieve);
+
 router
-  .route('/companies/:id')
-  .get(CompaniesController.retrieve)
+  .route('/companies/:id/orders')
+  .post(AuthenticationGuard, AccessGuard('marketplace'), OrdersController.create);
 
 router
   .route('/companies/orders/:id/accept')
