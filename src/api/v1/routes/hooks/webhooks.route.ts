918bb10a1cabd1cd13f195b022613fcbcd14a60c
Henrique Fonseca <henrique@fonseca.email>
2023-07-12 19:20:52 +0100

    feature(stripe): add script to automatically start stripe webhooks on development environment

--- src/api/v1/routes/hooks/webhooks.route.ts
+++ src/api/v1/routes/hooks/webhooks.route.ts
@@ -2,13 +2,11 @@
 import express from 'express';
 
 // Import Controller
-import StripeWebhooksController from '../../controllers/hooks/stripeWebHook.controller';
+import StripeWebhooksController from '../../controllers/hooks/stripe-webhooks.controller';
 const router = express.Router();
 
 router
-  .route('/webhooks/stripe/connect')
+  .route('/webhooks/stripe')
   .post(express.raw({ type: 'application/json' }), StripeWebhooksController.connect);
-
-router.route('/webhooks/stripe/account').post(StripeWebhooksController.account);
 
 export default router;
