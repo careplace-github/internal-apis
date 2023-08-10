2a072f27411949052473bacbf6eade932c7c2097
Henrique Fonseca <henrique@fonseca.email>
2023-08-10 21:53:44 +0100

    fix(leads): add missing leads route to the server

--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -14,3 +14,4 @@ export { default as PatientsRoute } from './patients.route';
 export { default as PaymentsRoute } from './payments.route';
 export { default as ReviewsRoute } from './reviews.route';
 export { default as ServicesRoute } from './services.route';
+export { default as LeadsRoute } from './leads.route';
