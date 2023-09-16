9d287a6f12ef94f8d2fdb83cabf35a5788071566
Henrique Fonseca <henrique.fonseca@careplace.pt>
2023-09-16 16:17:58 +0100

    feat(health units): add search endpoint

--- src/api/admin/v1/controllers/health-units.controller.ts
+++ src/api/admin/v1/controllers/health-units.controller.ts
@@ -429,4 +429,209 @@ export default class AdminHealthUnitsController {
       return next(new HTTPError._500(error.message));
     }
   }
+
+  static async adminSearchHealthUnits(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const response: IAPIResponse = {
+        statusCode: res.statusCode,
+        data: {},
+      };
+
+      const filters: FilterQuery<IHealthUnit> = {};
+      const options: QueryOptions<IHealthUnit> = {};
+
+      const page = typeof req.query.page === 'string' ? parseInt(req?.query?.page) : 1;
+      const documentsPerPage =
+        typeof req.query.documentsPerPage === 'string'
+          ? parseInt(req?.query?.documentsPerPage)
+          : 10;
+
+      // If the sortBy query parameter is not null, then we will sort the results by the sortBy query parameter.
+      if (req.query.sortBy) {
+        if (req.query.sortBy === 'rating') {
+          // sort by healthUnit.rating.average
+          options.sort = {
+            'rating.average': req.query.sortOrder === 'desc' ? -1 : 1, // 1 = ascending, -1 = descending
+          };
+        }
+        if (req.query.sortBy === 'price') {
+          // sort by healthUnit.business_profile.average_hourly_rate
+          options.sort = {
+            'pricing.minimum_hourly_rate': req.query.sortOrder === 'desc' ? -1 : 1, // 1 = ascending, -1 = descending
+          };
+        }
+
+        if (req.query.sortBy === 'name') {
+          // sort by healthUnit.business_profile.name
+          options.sort = {
+            'business_profile.name': req.query.sortOrder === 'desc' ? -1 : 1, // 1 = ascending, -1 = descending
+          };
+        }
+
+        if (req.query.sortBy === 'relevance') {
+          // sort by healthUnit.business_profile.name
+          options.sort = {
+            'business_profile.name': req.query.sortOrder === 'desc' ? -1 : 1, // 1 = ascending, -1 = descending
+          };
+        } else {
+          // If the sortOrder query parameter is not null, then we will sort the results by the sortOrder query parameter.
+          // Otherwise, we will by default sort the results by ascending order.
+          options.sort = {
+            [req.query.sortBy as string]: req.query.sortOrder === 'desc' ? -1 : 1, // 1 = ascending, -1 = descending
+          };
+        }
+      } else {
+        // If the sortBy query parameter is not provided, then we will sort the results by rhw name in an ascending order.
+        options.sort = {
+          'business_profile.name': req.query.sortOrder === 'desc' ? -1 : 1, // 1 = ascending, -1 = descending
+        };
+      }
+
+      // If the lat and lng query parameters are provided, we'll add them to the filter object.
+      if (req.query.lat && req.query.lng) {
+        // Convert the lat and lng query parameters to floats.
+        const lat = parseFloat(req.query.lat as string);
+        const lng = parseFloat(req.query.lng as string);
+
+        // If the lat and lng query parameters are valid numbers, then we'll add them to the filter object.
+        if (!isNaN(lat) && !isNaN(lng)) {
+          filters.service_area = {
+            $geoIntersects: {
+              $geometry: {
+                type: 'Point',
+                coordinates: [lat, lng],
+              },
+            },
+          };
+        }
+      }
+
+      if (req.query.type) {
+        filters.type = req.query.type as string;
+      }
+
+      // If the services query parameter is provided, we'll add it to the filter object.
+      if (req.query.services) {
+        const services = Array.isArray(req.query.services)
+          ? req.query.services
+          : // req.query.services=1,2,3 so we'll split it by comma and convert it to an array of strings.
+            (req.query.services as string).split(',');
+
+        filters.services = {
+          $all: services,
+        };
+      }
+
+      // If the minRating query parameter is provided, we'll add it to the filter object.
+      if (req.query.minRating) {
+        // Convert the minRating query parameter to a float.
+        const minRating = Array.isArray(req.query.minRating)
+          ? (req.query.minRating[0] as string)
+          : (req.query.minRating as string);
+
+        filters['rating.average'] = {
+          ...filters['rating.average'],
+          $gte: parseFloat(minRating),
+        };
+      }
+
+      // If the maxRating query parameter is provided, we'll add it to the filter object.
+      if (req.query.maxRating) {
+        // Convert the maxRating query parameter to a float.
+        const maxRating = Array.isArray(req.query.maxRating)
+          ? (req.query.maxRating[0] as string)
+          : (req.query.maxRating as string);
+
+        filters['rating.average'] = {
+          ...filters['rating.average'],
+          $lte: parseFloat(maxRating),
+        };
+      }
+
+      // If the maxPrice query parameter is provided, we'll add it to the filter object.
+      if (req.query.maxPrice) {
+        // Convert the maxPrice query parameter to a float.
+        const maxPrice = Array.isArray(req.query.maxPrice)
+          ? (req.query.maxPrice[0] as string)
+          : (req.query.maxPrice as string);
+
+        filters['pricing.minimum_hourly_rate'] = {
+          ...filters['pricing.minimum_hourly_rate'],
+          $lte: parseFloat(maxPrice),
+        };
+      }
+
+      // If the minPrice query parameter is provided, we'll add it to the filter object.
+      if (req.query.minPrice) {
+        // Convert the minPrice query parameter to a float.
+        const minPrice = Array.isArray(req.query.minPrice)
+          ? (req.query.minPrice[0] as string)
+          : (req.query.minPrice as string);
+
+        filters['pricing.minimum_hourly_rate'] = {
+          ...filters['pricing.minimum_hourly_rate'],
+          $gte: parseFloat(minPrice),
+        };
+      }
+
+      // If the name query parameter is provided, we'll add it to the filter object.
+      if (req.query.name) {
+        const nameSubstring = req.query.name as string;
+
+        // Create a regular expression for a case-insensitive substring search.
+        const nameRegex = new RegExp(nameSubstring, 'i');
+
+        filters['business_profile.name'] = {
+          $regex: nameRegex,
+        };
+      }
+
+      const healthUnits = await AdminHealthUnitsController.HealthUnitsDAO.queryList(
+        filters,
+        options,
+        page,
+        documentsPerPage,
+        undefined
+      );
+
+      // for each health unit get the stripe account
+      const healthUnitsWithStripeAccount = await Promise.all(
+        healthUnits.data.map(async (healthUnit) => {
+          const connectAccountId = healthUnit.stripe_information?.account_id;
+          let connectAccount: Stripe.Account | undefined;
+          try {
+            connectAccount = await AdminHealthUnitsController.StripeService.retrieveConnectAccount(
+              connectAccountId
+            );
+          } catch (error: any) {
+            switch (error.type) {
+              default:
+                //return next(new HTTPError._500(error.message));
+            }
+          }
+
+          return {
+            ...healthUnit.toJSON(),
+            stripe_account: connectAccount,
+          };
+        })
+      );
+
+      response.statusCode = 200;
+      response.data = {
+        ...healthUnits,
+        data: healthUnitsWithStripeAccount,
+      };
+
+      // Pass to the next middleware to handle the response
+      next(response);
+    } catch (error: any) {
+      // Pass to the next middleware to handle the error
+      return next(new HTTPError._500(error.message));
+    }
+  }
 }
